3ffc3852de7abc5c66b0e6871d87b657fef4b913
Anna Jordanous <annajordanous@hotmail.com>
2016-08-04 11:43:29 +0100

    starting to sort images out

---
+++ plosOneJordanousKeller/PAPER/images/flow_non_cc.mm
@@ -0,0 +1,103 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1470219957233" ID="ID_1781144173" MODIFIED="1470303814860" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Search strategy for
+    </p>
+    <p>
+      Creativity Corpus
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1470301927136" ID="ID_1422956260" MODIFIED="1470303942669" POSITION="right" VSHIFT="1">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Identify and add to the corpus those papers using a literature search on the nature of creativity, using the Scopus database</b>
+    </p>
+    <ul>
+      <li>
+        Papers should be available as full text, in English, and in PDF form or other format which it is straightforward to extract text from
+      </li>
+      <li>
+        Choose papers according to the following inclusion criteria (in order of descending precedence):
+
+        <ul>
+          <li>
+            Paper content should primarily focus on discussion of the nature of creativity
+
+            <ul>
+              <li>
+                Use titles and abstracts to identify candidates
+              </li>
+              <li>
+                Check content of candidate paper to refine by content
+              </li>
+            </ul>
+          </li>
+          <li>
+            Paper should be particularly influential
+
+            <ul>
+              <li>
+                Compare citation counts, preferring papers with higher citations than their disciplinary/time-based contemporaries
+              </li>
+              <li>
+                For the most recent papers (less time to accrue citations) make subjective judgement of influence based on a knowledge of the field
+              </li>
+            </ul>
+          </li>
+          <li>
+            Give preference to a selection of papers (where reasonable) which represent a cross-section of years since 1950
+          </li>
+          <li>
+            Give preference to a selection of papers (where reasonable) which represent a cross-section of disciplines relevant to the discussion of the nature of creativity
+          </li>
+        </ul>
+      </li>
+      <li>
+        Exclude papers where the author is already represented in the corpus (using the most influential/highly cited paper from that author in the corpus, as judged above) - unless:
+
+        <ul>
+          <li>
+            The two papers address different perspectives on creativity
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1470303355123" ID="ID_1080385774" MODIFIED="1470303945501" VSHIFT="2">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Identify and add to the corpus those papers which have had significant influence in discussions on the nature of creativity, which have not appeared in the Scopus literature search</b>
+    </p>
+    <ul>
+      <li>
+        Full peer-reviewed conference papers that are not archived by Scopus but which have had influence in their discipline such as a high number of citations (identified through a knowledge of the field)
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
---
+++ plosOneJordanousKeller/PAPER/images/flow_non_non_cc.mm
@@ -0,0 +1,51 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1470219957233" ID="ID_1781144173" MODIFIED="1470304010802" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Search strategy for
+    </p>
+    <p>
+      Non-Creativity Corpus
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1470304081541" ID="ID_1050021359" MODIFIED="1470304312412" POSITION="right" TEXT="For each paper in the creativity corpus:">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1470304169830" ID="ID_1037060222" MODIFIED="1470304379146">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Search on the Scopus database to retrieve the <b>two</b>&#160;most-cited papers in the same academic discipline(s) and year as the creativity corpus paper, for which:
+    </p>
+    <ul>
+      <li>
+        The paper is written in English
+      </li>
+      <li>
+        The paper is available in PDF or other format from which it is straightforward to extract text
+      </li>
+      <li>
+        The paper content does not contain any words with the prefix <i>creat-</i>&#160; (i.e. creativity, creative, creation, and so on)
+      </li>
+    </ul>
+    <p>
+      <b>Add these two papers to the non-creativity corpus.</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
